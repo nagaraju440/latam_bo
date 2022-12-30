16d35f345fbdb938ad16a67e2af30b6794fd99c0
supraja <karumurisupraja@gamil.com>
2022-12-30 14:37:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Notification from './components/Section2/Notifications'
+import Cards from './components/Section2/Cards'
 
 function App() {
   return (
     <div>
-      <div  className='text-xl bg-green-500' >hello tailwind text</div>
+      {/* <div  className='text-xl bg-green-500' >hello tailwind text</div> */}
+      {/* <Notification /> */}
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Section2/Notifications.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import bell from '../assets/bell.svg'
+function notification() {
+    const Data = [
+        {  id : '1',
+           text:"Lorem ipsum dolor sit amet, consectetur",
+           
+        },
+        {  id : '2',
+           text:"Lorem ipsum dolor sit amet, consectetur",
+           
+        },
+        {  id : '3',
+           text:"Lorem ipsum dolor sit amet, consectetur",
+        },
+        {  id : '4',
+           text:"Lorem ipsum dolor sit amet, consectetur",
+           
+        },
+        
+        ]
+  return (
+   <div className='m-10'>
+    <div className=' border border-[#D7D7D7] h-72 w-96 p-7 rounded  flex flex-col'>
+       <div className='flex flex-row gap-3'>
+            <img src={bell} alt='this is notification svg'></img>
+            <p className='text-[21px] font-semibold text-[#59595B]'>Notifications</p>
+         </div>
+        {Data.map((item)=>{
+                return(
+               
+                        <div className='items-center '>
+                                <div className='flex justify-center text-[16px] text-[#59595B] font-normal leading-9'>{item.text}</div>
+                                <hr></hr>
+                        </div>
+                  
+                
+                )
+                })}
+
+        </div>
+        </div>
+
+  )
+}
+
+export default notification
