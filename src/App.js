afbe400dfb94820b9afd93d010c1bb4aba1c8f75
bhuvaneshsai <bhuvaneshsai24@gmail.com>
2023-01-02 12:55:55 +0530

    added modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Modal from "./Modal";
+  import { useState } from 'react';
 
 function App() {
+  const [showModal,setshowModal]=useState(false);
+  const handleOnClose=()=>setshowModal(false);
+
   return (
     <div>
-      <div  className='text-xl bg-green-500' >hello tailwind text</div>
+      {/* <div  className='text-xl bg-green-500 font-bold' >hello tailwind text</div> */}
+      <div className="maindiv">
+        <div className="headerbutton p-3">
+          <button 
+          onClick={()=>setshowModal(true)}
+          className="w-25 bg-slate-400 h-12 text-sm text-center p-3 float-right rounded">click here</button>
+        </div>
+      </div> 
+    <Modal onClose={handleOnClose} visible={showModal}/>
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Modal({ visible, onClose }) {
+  if (!visible) return null;
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center  ">
+      <div
+        className="   card bg-white p-3 rounded-md w-[500px] h-[500px] mt-12 "
+      >
+        <div className="font-bold">
+          Modal sample
+          <button
+            className="float-right p-1 hover:animate-pulse"
+            onClick={onClose}
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
