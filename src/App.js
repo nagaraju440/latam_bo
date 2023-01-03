74cd5dc50590f4964ce7f3f838554f9082831a02
ajaydarisi <ajaydarisi@ajaydarisis-MacBook-Air.local>
2023-01-03 16:50:56 +0530

    Resume Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import './App.css';
 import ModalButton from './Components/ModalButton';
+import Resume from './Components/Resume/Resume';
 
 function App() {
   return (
     <div className="App font-body">
       <ModalButton />
+      {/* <Resume/> */}
     </div>
   );
 }
