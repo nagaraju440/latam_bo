db98a7a1da1181ae3c39a04aa69c94d98e312e31
ajaydarisi <ajaydarisi@ajaydarisis-MacBook-Air.local>
2023-01-03 17:48:16 +0530

    Modal modified

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import Resume from './Resume/Resume';
+import React from "react";
+import { ReactComponent as CloseButton } from "../Assets/Close.svg";
+import Resume from "./Resume/Resume";
 
-export default function Modal({visible,onClose}) {
-    if(!visible) return null;
+export default function Modal({ visible, onClose, children }) {
+  if (!visible) return null;
   return (
-        <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
-            <div className='card bg-white p-3 rounded-md w-1/3 h-3/4 flex flex-col justify-between '>
-              <div className="font-bold">Modal sample
-              <button className="float-right p-1" onClick={onClose}>X</button> 
-              </div>
-              <div className='flex align-center justify-center'>
-                <Resume/>
-              </div>
-            </div>
+    <div className="fixed inset-0 bg-black bg-opacity-30 ">
+      <div className="card relative bg-white rounded-md w-[650px] mx-auto py-5 min-h-[330px] mt-12">
+        <div className="font-bold">
+          <button className="flex ml-auto p-2 top-1 right-1 absolute right-0" onClick={onClose}>
+            <CloseButton />
+          </button>
+          <div className="px-12">
+            <Resume />
+          </div>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
