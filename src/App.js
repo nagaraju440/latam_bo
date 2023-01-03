14ef89871f47de3cd129d31ad203a644e0fc5669
bhargav466 <mvsbhargav466@gmail.com>
2023-01-03 22:51:44 +0530

    added select box code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NewCourse from './pages/instructor/courses/new';
 import Modal from "./Modal";
 import StepIndicator from './pages/instructor/courses/StepIndicator/StepIndicator';
   import { useState } from 'react';
+import Select from "./select.js";
 
 function App() {
   const [showModal,setshowModal]=useState(false);
@@ -10,15 +11,16 @@ function App() {
 
   return (
     <div>
-      <div className="maindiv">
+      {/* <div className="maindiv">
         <div className="headerbutton p-3">
           <button 
           onClick={()=>setshowModal(true)}
           className="w-25 bg-slate-400 h-12 text-sm text-center p-3 float-right rounded">click here</button>
         </div>
       </div> 
-    <Modal onClose={handleOnClose} visible={showModal}><NewCourse/></Modal>
-    
+    <Modal onClose={handleOnClose} visible={showModal}><NewCourse/></Modal> */}
+    {/* <Select/> */}
+    <Select></Select>
     </div>
   );
 }
