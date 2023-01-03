985efc70518dcab3121d86cb97566aa139c953f5
bala nagaraju <payyavula@payatu.io>
2023-01-03 15:33:17 +0530

    integrated modal step and inputs

--- src/pages/instructor/courses/new/index.js
+++ src/pages/instructor/courses/new/index.js
@@ -1,11 +1,39 @@
+import Stepper from "../StepIndicator/StepIndicator";
 import NewCoursePage1 from "./NewCoursePage1";
 
 const NewCourse=()=>{
     return(
-        <div >
-            <NewCoursePage1/>
+        <div>
+            {/* <NewCoursePage1/> */}
+            <Stepper  stepsData={stepsData} />
         </div>
     )
 }
 
 export default NewCourse;
+
+
+const stepsData = [
+  {
+    header: "header 1",
+    descp: "description 1",
+    stepComponent: <NewCoursePage1 />,
+  },
+  {
+    header: "header 2",
+    descp: "description 2",
+    stepComponent: <NewCoursePage1 />,
+    
+  },
+  {
+    header: "header 3",
+    descp: "description 1",
+    stepComponent: <NewCoursePage1 />,
+  },
+  {
+    header: "header 4",
+    descp: "description 4",
+    stepComponent: <NewCoursePage1 />,
+  },
+
+];
