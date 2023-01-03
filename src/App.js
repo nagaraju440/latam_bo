0f1bb7b70b960985986fddbf8e533e3ec99c35e7
Uman <uman.tab12@gmail.com>
2023-01-03 08:46:36 +0530

    StepIndicator commits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NewCourse from './pages/instructor/courses/new';
 import Modal from "./Modal";
+import StepIndicator from './pages/instructor/courses/StepIndicator/StepIndicator';
   import { useState } from 'react';
 
 function App() {
@@ -12,14 +13,15 @@ function App() {
       {/* <NewCourse/> */}
       {/* <div  className='text-xl bg-green-500' >hello tailwind text</div> */}
       {/* <div  className='text-xl bg-green-500 font-bold' >hello tailwind text</div> */}
-      <div className="maindiv">
+      {/* <div className="maindiv">
         <div className="headerbutton p-3">
           <button 
           onClick={()=>setshowModal(true)}
           className="w-25 bg-slate-400 h-12 text-sm text-center p-3 float-right rounded">click here</button>
         </div>
       </div> 
-    <Modal onClose={handleOnClose} visible={showModal}/>
+    <Modal onClose={handleOnClose} visible={showModal}/> */}
+    <StepIndicator />
     </div>
   );
 }
