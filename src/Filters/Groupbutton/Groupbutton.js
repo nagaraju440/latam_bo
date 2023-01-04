3f2e68ec90503c48bdf94e4df0e2a8e9bdb2182d
priyasura <priyasura01@gmail.com>
2023-01-04 14:01:39 +0530

    update

--- src/Filters/Groupbutton/Groupbutton.js
+++ src/Filters/Groupbutton/Groupbutton.js
@@ -4,7 +4,7 @@ function ButtonGroup({ buttons }){
   return (
     <div className="flex py-1">
       {buttons.map((buttonLabel, i) => (
-        <button key={i} name={buttonLabel} onClick={() => setClickedId(i)} className={i === clickedId ? "flex text-[#858585] px-5 py-2  bg-[#EEEEEE] " : "flex bg-[#ffffff] text-[#858585] px-5 py-2 border-[#FFFFFF]"} >
+        <button key={i} name={buttonLabel} onClick={() => setClickedId(i)} className={i === clickedId ? "flex text-[#858585] px-3 py-2  bg-[#EEEEEE] border-none" : "flex bg-[#ffffff] text-[#858585] px-3 py-2 border-none"} >
           {buttonLabel}
         </button>
       ))}
--- src/Filters/Section2.js
+++ src/Filters/Section2.js
@@ -9,77 +9,38 @@ function Section2() {
         //do some stuff here
     };
     return (
-        <div className=' bg-[#E5E5E5] justify-center items-center pt-9 pb-5   grid grid-cols-5' >
+        <div className='bg-[#E5E5E5] justify-center items-center px-12 py-5   grid grid-cols-5 gap-3' >
             <div>
-         {/* first */}
-         <div className="px-1 rounded-[5px] bg-[#ffffff] ">
-                        <ButtonGroup
-                            buttons={["En línea", "Presencial"]}
-                            doSomethingAfterClick={printButtonLabel}
-                        />
-                    </div>
+                <div className="rounded-[5px] bg-[#ffffff] w-48 ">
+                    <ButtonGroup
+                        buttons={["En línea", "Presencial"]}
+                        doSomethingAfterClick={printButtonLabel}
+                    />
+                </div>
             </div>
-            <div className=''>
-            <div className=' px-3 py-3 rounded-[5px] bg-[#ffffff] justify-between items-center text-center '>
-                         <div>
-                             <img src={CalenderIcon}></img>
-                         </div>
-                         <div className='pl-4 text-sm font-normal text-[#898989]'>Filtrar por fecha</div>
-                 <div >
-                             <img src={Dropdownicon}></img>
-                         </div>
-                     </div>
-{/* second */}
+            <div className='flex px-3 py-3.5 rounded-[5px] bg-[#ffffff] justify-between items-center text-center w-48'>
+                <div>
+                    <img src={CalenderIcon}></img>
+                </div>
+                <div className='flex pl-1 text-sm font-normal text-[#898989]'>Filtrar por fecha</div>
+                <div >
+                    <img src={Dropdownicon}></img>
+                </div>
             </div>
-            <div className=' '>
-{/* third */}
 
             <div >
-                          <EstatusComponent></EstatusComponent>
-                     </div>
+                <EstatusComponent></EstatusComponent>
             </div>
-            <div className=' '>
-fourth
-            </div>
-            <div className=' '>
-{/* fifth */}
-<div className=" justify-center items-center col-span-2">
-                <div className='text-sm font-normal text-[#898989] underline'>Más opciones</div>
-        {/* //             <div></div> */}
-               </div>
+            <div>
+
             </div>
 
+            <div className=' '>
+                <div className=" justify-center items-center col-span-2 pl-6">
+                    <div className='text-sm font-normal text-[#898989] underline'>Más opciones</div>
+                </div>
+            </div>
         </div>
-        // <div className=" bg-[#E5E5E5] justify-center items-center pt-9 pb-5">
-        //     <div className=" bg-red justify-between w-11/12 grid grid-cols-15">
-        //     {/* className="flex justify-between " */}
-        //         {/* <div  className="grid grid-cols-13"> */}
-        //             <div className="px-1 rounded-[5px] bg-[#ffffff] col-span-3">
-        //                 <ButtonGroup
-        //                     buttons={["En línea", "Presencial"]}
-        //                     doSomethingAfterClick={printButtonLabel}
-        //                 />
-        //             </div>
-        //             <div className=' px-3 py-3 rounded-[5px] bg-[#ffffff] justify-between items-center text-center col-span-3'>
-        //                 <div>
-        //                     <img src={CalenderIcon}></img>
-        //                 </div>
-        //                 <div className='pl-4 text-sm font-normal text-[#898989]'>Filtrar por fecha</div>
-        //                 <div className='pl-12'>
-        //                     <img src={Dropdownicon}></img>
-        //                 </div>
-        //             </div>
-        //             <div className='col-span-2'>
-        //                  <EstatusComponent></EstatusComponent>
-        //             </div>
-        //             {/* <div className='col-span-4'></div> */}
-        //         {/* </div> */}
-        //         <div className="bg-[blue] justify-center items-center col-span-2">
-        //             <div className='text-sm font-normal text-[#898989] underline'>Más opciones</div>
-        //             <div></div>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
