cfd054f7ee8aafc9f23b8cd9bb03fb603b3fa47a
bhuvaneshsai <bhuvaneshsai24@gmail.com>
2023-01-04 10:02:25 +0530

    added newcourse page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import './App.css';
 import NewCourse from './pages/instructor/courses/new';
 import Modal from "./Modal";
 import StepIndicator from './pages/instructor/courses/StepIndicator/StepIndicator';
-<<<<<<< HEAD
 import { useState } from 'react';
 import NormalCloseSvg from "./assets/icons/NormalCloseIcon";
-
-=======
-  import { useState } from 'react';
 import Select from "./select.js";
->>>>>>> 14ef89871f47de3cd129d31ad203a644e0fc5669
 
 function App() {
   const [showModal,setshowModal]=useState(false);
