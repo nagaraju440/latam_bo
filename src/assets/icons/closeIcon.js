bf55dbc29a40fa78b4a814233e8a3c261a6a726b
jhansinagalla <jhansinagalla05@gmail.com>
2023-01-04 15:48:52 +0530

    stepfourcompleted

--- src/assets/icons/closeIcon.js
+++ src/assets/icons/closeIcon.js
@@ -1,3 +1,5 @@
+
+
 const CloseIconSvg=(props)=>{
   return (
     <svg
@@ -15,6 +17,8 @@ const CloseIconSvg=(props)=>{
         d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
       />
     </svg>
+    
+    
   );
 }
 export default CloseIconSvg;
