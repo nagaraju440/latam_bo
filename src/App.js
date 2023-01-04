4f4ebc77b921e6f5c212265297e7deb6c9c1e9d9
kalikayiCode <sunilkalikayiofficial@gmail.com>
2023-01-04 10:39:56 +0530

    dash board tamplate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Cards from "./components/Section2/Cards";
-import Notification from "./components/Section2/Notifications";
-import Sidebar from "./components/Sidebar";
 import { Dashbaord } from "./pages/Dashbaord";
 
 function App() {
-  return (
-    <div>
-      {/* <div  className='text-xl bg-green-500' >hello tailwind text</div> */}
-      <Notification />
-      <Cards />
-    </div>
-  );
+    return (
+        <div>
+            <Dashbaord />
+            
+        </div>
+    );
 }
 
 export default App;
