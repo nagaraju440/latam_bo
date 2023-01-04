7ab53feae8ef8b7e737f3bab92d57bfe6cf4fe54
Karanam-Tejaswini <tejaswinikranam5@gamil.com>
2023-01-04 10:56:30 +0530

    seventh

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import "./App.css";
 import { Dashbaord } from "./pages/Dashbaord";
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <Navbar/>
-      <Sidebar/>
-      {/* <div  className='text-xl bg-green-500' >hello tailwind text</div> */}
-      {/* <Notification /> */}
-      {/* <Cards /> */}
-    </div>
-  );
-=======
     return (
         <div>
-            <Dashbaord />
-            
+            <Dashbaord /> 
         </div>
     );
->>>>>>> 0f8dd54175936ad630268a39622316abe4d5b639
 }
 
 export default App;
--- src/pages/Dashbaord.js
+++ src/pages/Dashbaord.js
@@ -4,10 +4,10 @@ import Sidebar from "../components/Sidebar";
 
 export const Dashbaord = () => {
     return (
-        <div className="border h-screen w-screen  bg-red-200">
-            <div className="bg-blue-200 h-20"> </div>
+        <div className="border h-screen w-screen  ">
+            <div className="h-20"><Navbar/></div>
             <div className="bg-yellow-200 grid grid-cols-10 h-full">
-                <div className="bg-green-200 col-span-2">side nav </div>
+                <div className="col-span-2"><Sidebar/> </div>
                 <div className="bg-grey-200 col-span-5"> middle part</div>
                 <div className="bg-orange-200 col-span-3">end part</div>
             </div>
