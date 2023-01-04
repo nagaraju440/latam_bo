0f8dd54175936ad630268a39622316abe4d5b639
kalikayiCode <sunilkalikayiofficial@gmail.com>
2023-01-04 10:51:08 +0530

    dashboard template

--- src/pages/Dashbaord.js
+++ src/pages/Dashbaord.js
@@ -6,9 +6,9 @@ export const Dashbaord = () => {
     return (
         <div className="border h-screen w-screen  bg-red-200">
             <div className="bg-blue-200 h-20"> </div>
-            <div className="bg-yellow-200 grid grid-cols-9">
+            <div className="bg-yellow-200 grid grid-cols-10 h-full">
                 <div className="bg-green-200 col-span-2">side nav </div>
-                <div className="bg-grey-200 col-span-4"> middle part</div>
+                <div className="bg-grey-200 col-span-5"> middle part</div>
                 <div className="bg-orange-200 col-span-3">end part</div>
             </div>
         </div>
