ea987e4458988e542eb7a040540328fa1c03ae68
Praveen <dpraveen453@gmail.com>
2023-01-04 15:20:51 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Dashbaord } from "./pages/Dashbaord";
-import Carouselcards from "./components/Section2/Carouselcards";
+import Carouselcards from "./components/Section1/Carouselcards";
 // import Introcard from "./components/Section2/Introcard";
 function App() {
     return (
--- src/pages/Dashbaord.js
+++ src/pages/Dashbaord.js
@@ -4,6 +4,7 @@ import Sidebar from "../components/Sidebar";
 import Notifications from '../components/Section2/Notifications'
 import Cards from '../components/Section2/Cards'
 import Card from "../components/Section1/Card";
+import RecentCardCarousel from "../components/Section1/RecentCardCarousel/RecentCardCarousel";
 
 export const Dashbaord = () => {
     return (
@@ -11,10 +12,11 @@ export const Dashbaord = () => {
             <div className="h-20"> <Navbar/></div>
             <div className="grid grid-cols-10">
                 <div className=" col-span-2"><Sidebar/> </div>
-                <div className="col-span-5 bg-[#F5F6F7] " > 
+                <div className="col-span-5 bg-[#F5F6F7] pt-[56px]" > 
                     <Card/>
+                    <RecentCardCarousel/>
                 </div>
-                <div className="col-span-3 bg-[#F5F6F7]">
+                <div className="col-span-3 bg-[#F5F6F7] pt-[56px]">
                     <Notifications/>
                     <Cards/>
                     </div>
