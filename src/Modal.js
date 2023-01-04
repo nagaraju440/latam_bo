c3b7698e830ed028fb2d1701002aa8ece40bfacf
bala nagaraju <payyavula@payatu.io>
2023-01-04 17:37:52 +0530

    worked on some stylings

--- src/Modal.js
+++ src/Modal.js
@@ -10,7 +10,7 @@ export default function Modal({ visible, onClose ,children }) {
   if (!visible) return null;
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 ">
-      <div className="card relative bg-white rounded-md w-[650px] mx-auto py-5 min-h-[330px] max-h-[90vh] mt-12 overflow-y-auto">
+      <div className="card relative bg-white rounded-[10px] w-[650px] mx-auto py-5 min-h-[330px] max-h-[90vh] mt-12 overflow-y-auto">
           <button className="flex ml-auto p-2 top-1 right-1 absolute " onClick={onClose}>
           <NormalCloseSvg/>
           </button>
